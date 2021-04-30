54c0b08940cd52b7066199d6d579737e9162ef33
carlodizon03 <carlodizon03@gmail.com>
2021-04-30 18:38:59 +0800

    update

--- Models/FibNet.py
+++ Models/FibNet.py
@@ -4,7 +4,7 @@ import torch.nn as nn
 import math
 from torchsummary import  summary
 from collections import defaultdict
-# from ptflops import get_model_complexity_info
+from ptflops import get_model_complexity_info
 
 class fibModule(nn.Module):
     '''
@@ -30,7 +30,7 @@ class fibModule(nn.Module):
         self.num_blocks = num_blocks
         self.block_depth = block_depth
         self.encoder = self.build(in_channels = self.in_channels, num_blocks = self.num_blocks, block_depth = self.block_depth)
-
+        
     def fibonacci(self,depth):
 
         f = []
@@ -41,7 +41,7 @@ class fibModule(nn.Module):
         return(f)
 
     def naive_block_channels_variation(self, blocks, in_channels = 3,  depth = 5, ratio = 0.618):
-        channel_list =[3]
+        channel_list =[in_channels]
         for block_idx, block in enumerate(blocks):
             depth_ = depth
             ratio_ = ratio 
@@ -51,64 +51,100 @@ class fibModule(nn.Module):
                 ratio_ = 3.414 * ratio_ * (1 - ratio_)
                 depth_ -= 1
         return channel_list   
-        
+
+    def convLayer(self, in_channels, out_channels, kernel_size = 3, stride = 1):
+        return nn.Sequential(nn.Conv2d(in_channels,out_channels,kernel_size,stride),
+                            nn.BatchNorm2d(out_channels),
+                            nn.ReLU())    
     def build(self, in_channels = 3, num_blocks = 5, block_depth = 5):
         initial_ratio = 0.618
         blocks_channel_list= self.naive_block_channels_variation(self.fibonacci(num_blocks),  in_channels, block_depth)
         encoder = nn.ModuleList()
+        
         for block in range(num_blocks):
             
             in_channels = blocks_channel_list[block*block_depth]
             out_channels = blocks_channel_list[block*block_depth+1]
-            encoder.append(nn.Conv2d(in_channels,
+            #Conv2d to match the shape for concatenation
+            encoder.append(self.convLayer(in_channels, 
+                                    in_channels, 
+                                    kernel_size = 3,
+                                    stride = 1))
+            #start of block conv
+            encoder.append(self.convLayer(in_channels,
                                     out_channels,
-                                    kernel_size=3))
+                                    kernel_size=3,
+                                    stride = 1))
             # print(block, 0, in_channels,out_channels)
             for layer in range(1,block_depth):
                 idx =  block*block_depth+layer
                 in_channels = blocks_channel_list[idx] + blocks_channel_list[idx-1]
                 out_channels = blocks_channel_list[idx+1]
-                encoder.append(nn.Conv2d(in_channels,out_channels,kernel_size=3))
+                encoder.append(self.convLayer(in_channels = blocks_channel_list[idx],
+                                              out_channels = blocks_channel_list[idx],
+                                              kernel_size=3))
+                encoder.append(self.convLayer(in_channels = in_channels,
+                                              out_channels = out_channels,
+                                              kernel_size=3))
+                # print(block, layer, blocks_channel_list[idx],blocks_channel_list[idx])
                 # print(block, layer, in_channels,out_channels)
                 if idx +1 == block_depth * num_blocks:
                     break
+        for idx, layer in enumerate(encoder):
+            print(idx, layer)
         return encoder
 
     def forward(self, inputs):
         x = inputs 
         for block in range(self.num_blocks):
-            out = self.encoder[block*self.block_depth](x)
+            # print('cat idx:{0} \t out_idx: {1}'.format(block*self.block_depth*2, block*self.block_depth*2+1))
+            cat_out = self.encoder[block*self.block_depth*2](x)
+            out = self.encoder[block*self.block_depth*2+1](x)
+            # print('block: {0} \t layer: {1} \t x: {2}  \t cat_out: {3} \t out: {4}'.format(block, 0, x.shape,  cat_out.shape, out.shape))
             for layer in range(1,self.block_depth):
-                print(x.shape, out.shape)
-                in2 = torch.cat((out,x),1)
+                in2 = torch.cat((out,cat_out),1)
                 x = out
-                out  = self.encoder[block*self.block_depth+layer](in2)
+                print('cat idx:{0} \t out_idx: {1}'.format(block*self.block_depth*2+(layer*2), block*self.block_depth*2+(layer*2)+1))
+                print(self.encoder[block*self.block_depth*2+(layer*2)])
+                print(self.encoder[block*self.block_depth*2+(layer*2)+1])
+                cat_out = self.encoder[block*self.block_depth*2+(layer*2)](x)
+                out  = self.encoder[block*self.block_depth*2+(layer*2)+1](in2)
+                print('block: {0} \t layer: {1} \t x: {2} \t in2:{3} \t cat_out: {4} \t out: {5}'.format(block, layer, x.shape, in2.shape, cat_out.shape, out.shape))
+                print(layer)
                 if layer == 4:
                     x = out
+                    print("later: 4 \t x:{0}".format(x.shape))
         return out
 
 class FibNet(nn.Module):
-    def __init__(self, in_channels = 3, out_channels = 1, num_blocks = 5, block_depth = 5, ):
+    def __init__(self, in_channels = 3, out_channels = 1, num_blocks = 5, block_depth = 5):
         super().__init__()
 
         self.in_channels = in_channels
         self.out_channels = out_channels
         self.num_blocks  = num_blocks
         self.block_depth = block_depth
-        self.encoder = fibModule() 
-        self.initial_conv = nn.Conv2d(in_channels = self.in_channels, out_channels = self.in_channels, kernel_size = 3, stride= 2)
+        self.conv1 = nn.Conv2d(3,16,3,2)
+        self.conv2 = nn.Conv2d(16,32,3,2)
+        self.encoder = fibModule(in_channels = 32, num_blocks = self.num_blocks, block_depth = self.block_depth)
+
     def forward(self, inputs):
-        inputs = self.initial_conv(inputs)
-        print("init_conv",inputs.shape)
+        inputs = self.conv1(inputs)
+        inputs = self.conv2(inputs)
         return self.encoder(inputs)
+# output shape = [(W−K+2P)/S]+1
 """Load Cuda """
 use_cuda = torch.cuda.is_available()
 device = torch.device("cuda:0" if use_cuda else "cpu")
 torch.backends.cudnn.benchmark = True
 """"""""""""""""""
-f = FibNet()
-f.to(device)
-
-# for p in f.parameters():
-#     print(p.shape)
+f = FibNet(num_blocks=3,block_depth=3)
+f.cuda()
+# x = torch.rand(1,3,224,224)
+# y = f(x)
+# print(y)
 summary(f,(3,224,224))
+macs, params = get_model_complexity_info(f, (3, 224, 224), as_strings=True,
+                                        print_per_layer_stat=True, verbose=False)
+print('{:<30}  {:<8}'.format('Computational complexity: ', macs))
+print('{:<30}  {:<8}'.format('Number of parameters: ', params))
